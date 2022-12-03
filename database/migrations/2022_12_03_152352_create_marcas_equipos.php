ea6882f9cfeb31f005090049cba5fb2555983fec
Gonzalo <g.castillo.dlf@gmail.com>
2022-12-03 12:38:03 -0300

    first commit

---
+++ database/migrations/2022_12_03_152352_create_marcas_equipos.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMarcasEquipos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Marcas_Equipos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Marcas_Equipos');
+    }
+}
